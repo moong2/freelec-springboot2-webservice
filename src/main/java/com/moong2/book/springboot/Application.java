3f6eafbc2c05f3c9ad32c98b61ff8e82b485af57
뭉이 <clapmean@gmail.com>
2021-02-20 18:44:21 +0900

    book-2

---
+++ src/main/java/com/moong2/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.moong2.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
