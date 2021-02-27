4bd0c8fd12991ff9bd3df6faaa2ccdf95823d980
뭉이 <clapmean@gmail.com>
2021-02-28 00:41:43 +0900

    login with Google

---
+++ src/main/java/com/moong2/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.moong2.book.springboot.config.auth.dto;
+
+import com.moong2.book.springboot.domain.posts.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
